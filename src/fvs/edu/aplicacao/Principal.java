ba235cfdd6f6d718bd45092055f5618c59ebffe6
Carlos Williamy <carloswilliamylourenco@gmail.com>
2018-09-04 19:51:38 -0300

    AulaGit2.1

--- src/fvs/edu/aplicacao/Principal.java
+++ src/fvs/edu/aplicacao/Principal.java
@@ -12,8 +12,11 @@ public class Principal {
 		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("topicos-fvs");
 		EntityManager em = emf.createEntityManager();
-		Aluno aluno = new Aluno(1, "Karlos Oliveira", "035.919.683-74", "132421312", 9131);
-		
+		em.getTransaction().begin();
+		Aluno aluno = new Aluno(null, "Carlos Williamy", "072.290.823-74", "arrozfeijao", 9558);
+		em.persist(aluno);
+		em.getTransaction().commit();
+		System.out.println("Salvo com sucesso!");
 		
 
 	}

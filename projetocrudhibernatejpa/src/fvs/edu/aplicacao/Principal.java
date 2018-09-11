762ae61e378853ebe19917950597849b0e3f0a4c
Carlos Williamy <carloswilliamylourenco@gmail.com>
2018-09-11 20:11:36 -0300

    Modelo Categoria

--- projetocrudhibernatejpa/src/fvs/edu/aplicacao/Principal.java
+++ projetocrudhibernatejpa/src/fvs/edu/aplicacao/Principal.java
@@ -12,12 +12,19 @@ public class Principal {
 		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("topicos-fvs");
 		EntityManager em = emf.createEntityManager();
-		Aluno aluno = em.find(Aluno.class,1);
+		
+		
+		Aluno aluno = em.find(Aluno.class, 3);
 		System.out.println(aluno);
+		
 		//em.getTransaction().begin();
-		//Aluno aluno = new Aluno(null, "Carlos Williamy", "072.290.823-74", "arrozfeijao", 9558);
+		//Aluno aluno = new Aluno(null, "Karlos Oliveira", "035.919.683-74", "132421312", 9131);
 		//em.persist(aluno);
 		//em.getTransaction().commit();
+		
+		System.out.println("Salvo com sucesso!");
+		
+
 	}
 
 }
